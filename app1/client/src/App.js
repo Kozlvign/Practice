671ace553c2f57b26ef13b816682f74f85003b15
Igor Nunes <63605496+Kozlvign@users.noreply.github.com>
2022-02-22 09:51:08 -0300

    Creating Jobz

---
+++ app1/client/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Jobz</h1>
+  );
+}
+
+export default App;
---
+++ quiz-app/script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old am I?',
+        a: '23',
+        b: '25',
+        c: '21',
+        d: '27',
+        correct: 'c'
+    }, {
+        question: 'What is the most used programming language om 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
+        question: 'What is the most used programming language om 2010?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    }, {
+        question: 'What is the most used programming language om 2010?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    }, {
+        question: 'What is the most used programming language om 2010?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    }
+]
